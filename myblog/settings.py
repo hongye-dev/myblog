d1c95219bbe8522f3c26d4b6eab8c3e390643532
yujun <18810230056@163.com>
2020-04-14 21:20:23 +0800

    修改ALLOWED_HOSTS S 为 **

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e4rf+n^gg2osh4r9=8ov2dq$jn%dej31ac%rgg=40pu)lfh#7n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
